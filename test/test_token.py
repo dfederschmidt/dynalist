e5616c466f1d0374115c488d5042fc59f34dba50
Daniel Federschmidt <daniel@federschmidt.xyz>
2018-09-03 11:29:49 +0200

    test new config

--- test/test_token.py
+++ test/test_token.py
@@ -2,7 +2,7 @@ import unittest
 
 class TestTokenMethods(unittest.TestCase):
     def test_set(self):
-        self.assertEqual('foo'.upper(), 'FOO')
+        self.assertEqual('foo'.upper(), 'FOp')
 
 if __name__ == "__main__":
     unittest.main()
